68cb939368eb33bb9fd4b2af371f21e5c23910e0
zaborch1k <ezaroch13@gmail.com>
2025-03-14 19:28:24 +0300

    all front

--- app.py
+++ app.py
@@ -13,22 +13,41 @@ def home():
 @app.route('/input', methods=['GET', 'POST'])
 def input():
     if request.method == 'POST':
-        return 'post'
+        from_api = False
+        data = request.form["user_input"]
+
+        if not data:
+            from_api = True
+        # бд
+
     return render_template('input.html')
 
 
 @app.route('/waypoints', methods=['GET', 'POST'])
 def waypoints():
     if request.method == 'POST':
-        return 'отдельная точка'
-    return render_template('waypoints.html')
+        waypoint = request.form['waypoint']
+        # данные от бд
+        data = {'waypoint': waypoint, 'fuel': 3, 'o2': 3}
+        return render_template('waypoint.html', data=data)
+
+    # данные от бд
+    waypoints_number = 4
+    return render_template('waypoints.html', waypoints=waypoints_number)
 
 
 @app.route('/days', methods=['GET', 'POST'])
 def days():
     if request.method == 'POST':
-        return 'отдельный день'
-    return render_template('days.html')
+        day = request.form['day']
+        # данные от бд
+        data = {'day': day, 'fuel': 4, 'o2': 4, 'power_for_engine': 4,
+                'power_for_electricity': 5, 'T': 3, 'sh': 5}
+        return render_template('day.html', data=data)
+
+    # данные от бд
+    days_number = 6
+    return render_template('days.html', days=days_number)
 
 
 app.run(debug=True)
